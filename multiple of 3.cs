a7117678a693765fd772fdb0791e8647e9590ad3
Anneldah <90136235+Anneldah@users.noreply.github.com>
2021-12-05 18:12:57 +0200

    Add files via upload

---
+++ multiple of 3.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int p=1;p<=12;p++)
+            {
+                Console.WriteLine("3*" + p + "=" + (3 * p));
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
