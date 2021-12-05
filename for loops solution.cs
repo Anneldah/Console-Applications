31d28346a000f52aa961e0cd0be0ac12c0f55ae8
Anneldah <90136235+Anneldah@users.noreply.github.com>
2021-12-05 18:10:48 +0200

    Add files via upload

---
+++ for loops solution.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numCust,cust=1;
+            double price, vat ,amtDue=0,total=0;
+            Console.Write("Enter the number of customers: ");
+            numCust = int.Parse(Console.ReadLine());
+            for(int custy=1; cust <= numCust; cust++)
+            {
+                Console.Write("Enter the price of item purchased" + cust + ":");
+                price = double.Parse(Console.ReadLine());
+                vat = price * 15 / 100.0;
+                amtDue = vat + price;
+                Console.WriteLine("\nAmout due is: " + amtDue.ToString("C"));
+                total += amtDue;
+            }
+            cust++;
+    
+            Console.WriteLine("\nGrand total ,ade by the store is: " +total.ToString("C"));
+            Console.ReadLine();
+        }
+        
+    }
+}
