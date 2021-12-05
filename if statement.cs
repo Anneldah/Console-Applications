086f8608734d18c747b284b036601c3daf09c16f
Anneldah <90136235+Anneldah@users.noreply.github.com>
2021-12-05 18:12:09 +0200

    Add files via upload

---
+++ if statement.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string does = "Knock";
+            string name = "Penny!";
+            if(does=="Knock"|| name=="Penny!")
+            {
+             
+                
+                    Console.WriteLine("Knock");
+                    Console.WriteLine("Knock");
+                    Console.WriteLine("Knock");
+                    Console.WriteLine("Penny!");
+
+                }
+                else
+                    Console.WriteLine("nothing");
+                    
+                Console.ReadKey();
+
+
+            
+        }
+    }
+}
